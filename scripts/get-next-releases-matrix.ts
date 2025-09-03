834ef1b32b2893fb6117f1d40f2309aa2148826b
Jarid Margolin <jaridmargolin@gmail.com>
2025-09-02 22:39:59 -0700

    fix: use docker manifests to ensure platform specific images pulled down

--- scripts/get-next-releases-matrix.ts
+++ scripts/get-next-releases-matrix.ts
@@ -9,26 +9,57 @@ import { getNextReleases } from "../lib/release-manager.js";
  * get-next-releases
  * -------------------------------------------------------------------------- */
 
-// Output the json string so that it can be consumed within our workflow
 const releases = await getNextReleases();
+const registryPrefixes = ["ghcr.io/devtemplates/"];
 
-// Convert to a simplified format that is intended to be consumed within as a
-// matrix in a GitHub workflow. Note that the matrix breaks up the releases by
-// platform because it will need to build separately for each platform
-const matrix = releases.flatMap(
-  (release) =>
-    release?.imagePlatforms.map((platform) => ({
-      DEVCONTAINER_BASE_IMAGE_TAG: release?.image.devcontainerTag.name,
-      DEVCONTAINER_DOCKER_VERSION: release?.image.dockerVersion,
-      DEVCONTAINER_NIX_VERSION: release?.image.nixVersion,
-      DEVCONTAINER_PACKAGE_VERSION: release?.image.packageVersion,
-      DEVCONTAINER_IMAGE_VERSION: release?.image.imageVersion,
-      DEVCONTAINER_IMAGE_NAME: release?.image.imageName,
-      DEVCONTAINER_PLATFORM: platform,
-      DEVCONTAINER_TAGS: release?.imageTags
-        .map((tag) => tag.split(":").pop())
-        .join(","),
-    })),
+console.log(
+  JSON.stringify({
+    /**
+     * A simplified array for the releases that is intended to be consumed
+     * upstream as a matrix in a GitHub workflow. Note that the matrix breaks up
+     * the releases by platform because it will need to build separately for
+     * each platform.
+     */
+    releases: releases.flatMap(
+      (release) =>
+        release?.imagePlatforms.map((platform) => ({
+          DEVCONTAINER_BASE_IMAGE_TAG: release?.image.devcontainerTag.name,
+          DEVCONTAINER_DOCKER_VERSION: release?.image.dockerVersion,
+          DEVCONTAINER_NIX_VERSION: release?.image.nixVersion,
+          DEVCONTAINER_PACKAGE_VERSION: release?.image.packageVersion,
+          DEVCONTAINER_IMAGE_VERSION: release?.image.imageVersion,
+          DEVCONTAINER_IMAGE_NAME: release?.image.imageName,
+          DEVCONTAINER_PLATFORM: platform,
+          DEVCONTAINER_TAGS: release?.imageTags
+            .map((t) => `${t.split(":").pop()}-${platform.replace("/", "-")}`)
+            .join(","),
+        })),
+    ),
+
+    /**
+     * An array of manfiest image URIs with accompanying aliases for each
+     * platform. This is required because each image is indepently built on
+     * optimized runners but we still want to allow users to pull from the base
+     * tag and get the platform optimized image. This is used upstream as a
+     * matrix in a GitHub workflow.
+     */
+    manifests: registryPrefixes.flatMap((registryPrefix) =>
+      releases.flatMap(
+        (release) =>
+          release?.imageTags.map((tag) => {
+            const imageUri = `${registryPrefix}${release?.image.imageName}:${tag
+              .split(":")
+              .pop()}`;
+            const alias = release?.imagePlatforms
+              .map((platform) => `${imageUri}-${platform.replace("/", "-")}`)
+              .join(" ");
+
+            return {
+              DEVCONTAINER_MANIFEST_IMAGE_URI: imageUri,
+              DEVCONTAINER_MANIFEST_ALIAS: alias,
+            };
+          }),
+      ),
+    ),
+  }),
 );
-
-console.log(JSON.stringify(matrix));
